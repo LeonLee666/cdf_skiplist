74f8e9f7a121286905889b4954dd5104d5607916
bombehub <liliang@stumail.neu.edu.cn>
2019-07-02 10:30:13 +0800

    merge repo to this repo

---
+++ README.md
@@ -0,0 +1,3 @@
+# AIskiplist
+
+通过**先验知识CDF**的结果，帮助skiplist的创建过程，从而有助于skiplist的查询速度。
---
+++ src/kde.py
@@ -0,0 +1,50 @@
+
+import numpy as np
+from scipy import stats
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+from statsmodels.distributions.mixture_rvs import mixture_rvs
+
+np.random.seed(12345)
+
+dist1_loc, dist1_scale, weight1 = -1 , 0.5, 0.25
+dist2_loc, dist2_scale, weight2 = 1 , 0.5, 0.75
+
+# 拟合样本array数据
+obs_dist = mixture_rvs(prob=[weight1, weight2], size=250, dist=[stats.norm, stats.norm],kwargs = (dict(loc=dist1_loc, scale=dist1_scale),dict(loc=dist2_loc, scale=dist2_scale)))
+
+# 核密度估计，可得到kde.support, kde.density, kde.cdf向量
+kde = sm.nonparametric.KDEUnivariate(obs_dist)
+kde.fit(kernel='gau')
+
+# 计算2.5的cdf值
+print(kde.cdf[np.searchsorted(kde.support,2.5)])
+fig = plt.figure(figsize=(12, 5))
+ax = fig.add_subplot(111)
+
+ax.plot(kde.support, kde.cdf, lw=3, label='CDF')
+true_cdf = (stats.norm.cdf(loc=dist1_loc, scale=dist1_scale, x=kde.support)*weight1+ stats.norm.cdf(loc=dist2_loc, scale=dist2_scale, x=kde.support)*weight2)
+ax.plot(kde.support, true_cdf, lw=3, label='True CDF', zorder=15)
+ax.hist(obs_dist, bins=50, cumulative=True, density=True, label='Histogram from samples',zorder=5, edgecolor='k', alpha=0.5)
+ax.legend(loc = 'best')
+ax.grid(True, zorder=-5)
+fig.show()
+
+# 样本集合
+#print(np.sort(obs_dist))
+
+#print(kde.support)
+#print(kde.ay)
+#print(true_cdf)
+
+#fig = plt.figure(figsize=(12, 5))
+#ax = fig.add_subplot(111)
+#true_values = (stats.norm.pdf(loc=dist1_loc, scale=dist1_scale, x=kde.support)*weight1+ stats.norm.pdf(loc=dist2_loc, scale=dist2_scale, x=kde.support)*weight2)
+#ax.hist(obs_dist, bins=20, density=True, label='Histogram from samples',zorder=5, edgecolor='k', alpha=0.5)
+#ax.plot(kde.support, kde.density, lw=3, label='KDE from samples', zorder=10)
+#ax.plot(kde.support, true_values, lw=3, label='True distribution', zorder=15)
+#ax.legend(loc='best')
+#ax.grid(True, zorder=-5)
+#fig.show()
+
+
