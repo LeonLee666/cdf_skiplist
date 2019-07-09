82002ed3a905b377fa11a71af3ee786c80ca496a
BombeHub <liliang@stumail.neu.edu.cn>
2019-07-09 11:06:11 +0800

    Create zipf.r

---
+++ cmake-build-debug/src/zipf.r
@@ -0,0 +1,16 @@
+library(methods)
+library(stats4)
+library(splines)
+library(VGAM)
+
+args=commandArgs(T)
+delta <- 0.1
+bound <- 64000000
+size <- 2097152
+x <- rzipf(size,bound,delta)
+# x <- sort(x)
+sink("dataset.txt")
+cdf <- ecdf(x)
+for( i in 1:size)
+	cat(x[i],",",cdf(x[i]),"\n")
+sink()
